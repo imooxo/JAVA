ab1b9584f3b14c626a97ac385d083be517942ecd
imooxo <kimsi1127@gmail.com>
2024-05-10 14:47:54 +0900

    DataType(primitive+reference)-24.05.10

--- java-lecture/src/_01_basic_syntax/DataTypes.java
+++ java-lecture/src/_01_basic_syntax/DataTypes.java
@@ -15,7 +15,7 @@ public class DataTypes {
         double f = 2.7777;
 
         // 문자형 변수 선언
-        char ch = 'A'; // "큰 따옴표X '작은 따옴표 O
+        char ch = 'A'; // 한단어 시 "큰 따옴표X '작은 따옴표 O / 문자시 "큰따옴표o
         
         // 논리형 변수 선언
         boolean bool = true;
@@ -24,5 +24,42 @@ public class DataTypes {
         System.out.println("실수형 변수:: "+e+" "+f);
         System.out.println("문자형 변수:: "+ch);
         System.out.println("논리형 변수:: "+bool);
+
+        System.out.println("----- reference type -----");
+        // 1. primitive에 있는 자료형
+        // 굳이 필요한 경우가 아니라면 primitive 형 사용
+        Integer aa = 1;;
+        Long bb = 10L;
+        Short cc = 128;
+        Byte dd = -128;
+        Float ee = 1.1F;
+        Double ff = 1.2;
+        Character chch = 'A';
+        Boolean boo = false;
+
+        // 2. primitive에 없는 자료형
+        String greeting = "Hello World!"; // 문자열
+        int[] numbers = {1,2,3,4,5}; // 배열
     }
 }
+
+// class (나중에 배울 예정)
+class Person {
+    private String name;
+
+    private int age;
+
+    public Person(String name, int age){  // 두개의 인자 받아줌
+      this.name = name;
+      this.age = age;
+    }
+
+    public  String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+}//class
